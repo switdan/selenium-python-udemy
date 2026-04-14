050f331ba16d43f1b1f3a92a92394d6e278c27ea
Daniel Świtała <switala.daniel@gmail.com>
2026-04-14 21:11:41 +0200

    dodanie funckji pytest_addoption w pliku contest.py i utworzenie możliwosci uruchamiania testow w przegladarce Firefox

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,20 @@ from selenium import webdriver
 
 
 @pytest.fixture()
-def driver():
-    print("Creating Chrome driver...")
-    my_driver = webdriver.Chrome()
+def driver(request):
+    browser = request.config.getoption("--browser")
+    print(f"Creating {browser} driver...")
+    if browser == "chrome":
+        my_driver = webdriver.Chrome()
+    elif browser == "firefox":
+        my_driver = webdriver.Firefox()
+    else:
+        raise Exception(f"Invalid browser. Expected 'chrome' or 'firefox', but got {browser}")
     yield my_driver
-    print("Closing Chrome driver.")
+    print(f"Closing {browser} driver.")
     my_driver.quit()
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="browser to execute tests (chrome, firefox)"
+    )
