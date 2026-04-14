6555907d028cc67e991aa2e79eb6120806d9b10a
Daniel Świtała <switala.daniel@gmail.com>
2026-04-14 21:20:13 +0200

    dodanie funckji pytest.fixture(params=['chrom', 'firefox']) w pliku contest.py by utworzyć możliwosć uruchamiania testów na wszystkich zainstalowanych przeglądarkach

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
 
-
+# Używane kiedy chcemy, aby wszystkie testy przeszły przez kąkretną przeglądarkę, wybrana w konfiguracji
 @pytest.fixture()
 def driver(request):
     browser = request.config.getoption("--browser")
@@ -15,6 +15,22 @@ def driver(request):
     yield my_driver
     print(f"Closing {browser} driver.")
     my_driver.quit()
+
+# Używane kiedy chcemy, aby wszystkie testy przeszły przez różne przeglądarki
+
+# @pytest.fixture(params=["chrome", "firefox"])
+# def driver(request):
+#     browser = request.param
+#     print(f"Creating {browser} driver...")
+#     if browser == "chrome":
+#         my_driver = webdriver.Chrome()
+#     elif browser == "firefox":
+#         my_driver = webdriver.Firefox()
+#     else:
+#         raise Exception(f"Invalid browser. Expected 'chrome' or 'firefox', but got {browser}")
+#     yield my_driver
+#     print(f"Closing {browser} driver.")
+#     my_driver.quit()
 
 def pytest_addoption(parser):
     parser.addoption(
