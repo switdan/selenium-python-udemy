45ddcdca74dbb163f6414afa390aec2ac62b5943
Daniel Świtała <switala.daniel@gmail.com>
2026-04-15 14:24:42 +0200

    stworzenie folderu 'page_objects' i pliku 'login_page' i dodanie lokatorow oraz funkcji z klasa LoginPage

---
+++ page_objects/login_page.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+
+class LoginPage:
+    __url = "https://practicetestautomation.com/practice-test-login/"
+    __username_field = (By.ID, "username")
+    __password_field = (By.ID, "password")
+    __submit_btn = (By.ID, "submit")
+
+    def __init__(self, driver: WebDriver):
+        self._driver = driver
+
+    def open(self):
+        self._driver.get(self.__url)
+
+    def execute_login(self, username: str, password: str):
+        wait = WebDriverWait(self._driver, 6)
+        wait.until(ec.visibility_of_element_located(self.__username_field))
+        self._driver.find_element(self.__username_field).send_keys(username)
+        wait.until(ec.visibility_of_element_located(self.__password_field))
+        self._driver.find_element(self.__password_field).send_keys(password)
+        wait.until(ec.visibility_of_element_located(self.__submit_btn))
+        self._driver.find_element(self.__submit_btn).click()
